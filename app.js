6db29bd507498e0aff570af37732dd00694ea578
Kihtrak <karthikandsankar@gmail.com>
2018-08-18 17:20:05 -0400

    added modal to hompage

--- app.js
+++ app.js
@@ -3,17 +3,36 @@ var provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().languageCode = 'pt';
 
 let signInBtn = document.getElementById("sign-in");
-
+let modalBtn =document.getElementById("navbarBtn");
+let modalLink =document.getElementById("navbarLink");
 
 console.log("TEST");
-signInBtn.addEventHandler("click", function(){
+signInBtn.addEventListener("click", function(){
     firebase.auth().signInWithRedirect(provider);
 });
 
 firebase.auth().onAuthStateChanged(function(user) {
   if(user){
     console.log("SIGNED IN");
+    let validUser = false;
+    firebase.database().ref('Authorized Members').on('value', function(snapshot) {
+      for (var i in snapshot.val() )
+        if(snapshot.val()[i]){
+          validUser = true;
+          break;
+        }
+      if(validUser){
+        window.location.href = "file:///C:/Users/karth/Documents/GitHub/750s/TeamInfo/index.html";
+      }else{
+
+      }
+    });
   }else{
     console.log("SIGNED OUT");
+    modalBtn.innerHTML = "For Team Members only";
   }
+});
+
+firebase.database().ref('Authorized Members').update({
+
 });
